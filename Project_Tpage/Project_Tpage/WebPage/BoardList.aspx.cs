de093210c9d063a11558e11a8e0f6eefd29b12f7
WeiZero <cd232310@gmial.com>
2018-12-27 16:29:51 +0800

    board to article list

--- Project_Tpage/Project_Tpage/WebPage/BoardList.aspx.cs
+++ Project_Tpage/Project_Tpage/WebPage/BoardList.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Project_Tpage.Class;
 
 namespace Project_Tpage.WebPage
 {
@@ -12,13 +13,19 @@ namespace Project_Tpage.WebPage
         // 資料庫在這邊給資料。顯示於listbox
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!Controller.IsConstrut)
+            {
+                //建構MVC
+                Controller.Initial(Session["state"] == null ? StateEnum.Board : (StateEnum)Session["state"]);
+            }
         }
 
         // 選擇要看文章
         protected void SelectArticle(object sender, EventArgs e)
         {
             int index = this.ListOfBoard.SelectedIndex;
+            Controller.controller.GetUserInput(ViewOp.Board_viewarticle);
+            Response.Redirect("ArticleList.aspx");
         }
     }
 }
