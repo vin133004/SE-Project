0bc3217e0506dd1f54e4e4284139f2144a952f09
WeiZero <cd232310@gmail.com>
2018-12-29 16:42:02 +0800

    view 1.3

---
+++ Project_Tpage/Project_Tpage/WebPage/Editor.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Project_Tpage.Class;
+
+namespace Project_Tpage.WebPage
+{
+    public partial class Editor : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Back_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void Setting_Click(object sender, EventArgs e)
+        {
+
+            // this.controller.;
+            // this.model.RequestPageData(controller.model.State);
+        }
+        protected void Send_Click(object sender, EventArgs e)
+        {
+            String content = Content.Text;
+            String tittle = Tittle.Text;
+
+        }
+    }
+}
