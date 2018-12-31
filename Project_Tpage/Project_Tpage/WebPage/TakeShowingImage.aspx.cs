7ebd92c6537b57284002a8a03c32e85b93ece74d
vints <vin133004@gmail.com>
2018-12-31 22:33:49 +0800

    provide method for showing image on webpage

---
+++ Project_Tpage/Project_Tpage/WebPage/TakeShowingImage.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using Project_Tpage.Class;
+
+namespace Project_Tpage.WebPage
+{
+    public partial class TakeShowingImage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Controller.CrossPageDAT.Keys.Contains("TEMP_ShowingImage"))
+                Response.BinaryWrite((byte[])new ImageConverter().
+                    ConvertTo(Controller.CrossPageDAT["TEMP_ShowingImage"], typeof(byte[])));
+        }
+    }
+}
